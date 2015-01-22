9cbe5d46b9229149964fd4be28bedf5eb0a00b07
Ian Driver <driver.ian@gmail.com>
2015-01-21 23:50:42 -0800

    opening with 'with' statements

--- open_read_close_file/file_opening.py
+++ open_read_close_file/file_opening.py
@@ -1,15 +1,42 @@
 #opening files
 
 #1st method
-#r = read w = write b = binary (useful on some systems)
-#o_file = open('path/to/filename', 'rwb')
+#r = read w = write b = binary (useful on some systems) + = editable, will write over existing
+#o_file = open('path/to/filename', 'rwb+')
 #file is now open and in memory as o_file
 #to close a file use: o_file.close()
 
 #example to play with:
 
-o_file = open('method1.txt', 'w+')
+o_file = open('method1.txt', 'rwb')
 print 'see that the open file is an object in memory:'
 print o_file
-o_file.write('First line\nSecond line\n')
+#write to file with .write I've already done this but the example is below
+#o_file.write('First line\nSecond line\n')
+
+#there are multiple ways to read a file but the easiest is to loop through it
+line_list = []
+for line in o_file:
+    #strip() will remove new line \n
+    l = line.strip()
+    line_list.append(l)
+    print l
+print line_list
+
+#with this method you must close the file or it will stay open in memory        
 o_file.close()
+
+#2nd method 
+#use a with statement to open the file, it will close automatically when done
+
+with open('method1.txt', 'rwb') as o2_file:
+    print 'see that the open file is an object in memory:'
+    print o2_file
+
+    line_list = []
+    for line in o2_file:
+        #strip() will remove new line \n
+        l = line.strip()
+        line_list.append(l)
+        print l
+    print line_list
